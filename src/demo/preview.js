799978b7ef81010d12bfe00722e2910d7a054a2d
Matthew McClure <m@mmcc.io>
2021-01-30 23:54:59 -0800

    adding a README

--- src/demo/preview.js
+++ src/demo/preview.js
@@ -1,7 +1,3 @@
-import hljs from 'highlight.js/lib/core';
-import javascript from 'highlight.js/lib/languages/javascript';
-hljs.registerLanguage('javascript', javascript);
-
 (() => {
   // nothing to see here yet.
 })();
