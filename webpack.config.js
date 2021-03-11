361abf7f1f100b6eb43e421f2dd0f2a4ae274ede
Matthew McClure <github@mmcc.io>
2021-03-10 23:37:09 -0800

    prettier.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-const { ESBuildPlugin, ESBuildMinifyPlugin } = require("esbuild-loader");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { ESBuildPlugin, ESBuildMinifyPlugin } = require('esbuild-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const dev = process.env.NODE_ENV !== "production";
+const dev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: {
     index: [
-      "./src/index.js",
-      "./src/demo/preview.js",
-      "./src/demo/preview.scss",
+      './src/index.js',
+      './src/demo/preview.js',
+      './src/demo/preview.scss',
     ],
   },
-  mode: dev ? "development" : "production",
+  mode: dev ? 'development' : 'production',
   watch: dev,
   module: {
     rules: [
       {
         test: /\.js$/,
-        loader: "esbuild-loader",
+        loader: 'esbuild-loader',
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
-          dev ? "style-loader" : MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
+          dev ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
         ],
       },
     ],
   },
   resolve: {
     alias: {
-      "video.js": "video.js/core",
+      'video.js': 'video.js/core',
     },
   },
   optimization: {
@@ -42,13 +42,13 @@ module.exports = {
   plugins: [
     new ESBuildPlugin(),
     new HtmlWebpackPlugin({
-      template: "src/demo/index.html",
+      template: 'src/demo/index.html',
       inject: true,
-      chunks: ["index"],
-      filename: "index.html",
+      chunks: ['index'],
+      filename: 'index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
+      filename: '[name].css',
     }),
   ],
 };
