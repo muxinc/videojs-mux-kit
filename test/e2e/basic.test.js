ee44de541dbf39be205b82f947d3eca997c6b5cb
Erik Peña <erikpena@users.noreply.github.com>
2021-03-31 15:20:25 -0700

    👉🧑‍💻👈 - Updated jest config to remove testEnvironment and add verbose logs - Refactored tests to used global page var - Refactored tests to be more DRY

--- test/e2e/basic.test.js
+++ test/e2e/basic.test.js
@@ -1,32 +1,5 @@
-const playwright = require('playwright');
-
 describe('Basic demo page', () => {
-  it('chromium should load', async () => {
-    const browser = await playwright['chromium'].launch();
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto('http://localhost:8080/basic.html');
-
-    expect(await page.$eval("h2", el => el.textContent)).toEqual('Basic example');
-    
-    await browser.close();
-  });
-  
-  it('firefox should load', async () => {
-    const browser = await playwright['firefox'].launch();
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    await page.goto('http://localhost:8080/basic.html');
-
-    expect(await page.$eval("h2", el => el.textContent)).toEqual('Basic example');
-    
-    await browser.close();
-  });
-  
-  it('webkit should load', async () => {
-    const browser = await playwright['webkit'].launch();
-    const context = await browser.newContext();
-    const page = await context.newPage();
+  it('should load', async () => {
     await page.goto('http://localhost:8080/basic.html');
 
     expect(await page.$eval("h2", el => el.textContent)).toEqual('Basic example');
