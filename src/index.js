e12ed898bf7a82ba2c1e8178a12647e614fd42f1
james-mux <james@mux.com>
2021-05-24 15:14:27 +0100

    signed urls support

--- src/index.js
+++ src/index.js
@@ -18,13 +18,27 @@ videojs.use('video/mux', (player) => {
     setSource({ src }, next) {
 
       if (player.options().timelineHoverPreviews) {
+        let storyboardUrl = `https://image.mux.com/${src}/storyboard.vtt`;
+        
+        // check if we have a storyboard token to use with signed playback URLs
+        if (player.options().timelineHoverPreviews.token) {
+          storyboardUrl = storyboardUrl + `?token=` + player.options().timelineHoverPreviews.token;
+        }
+
         player.vttThumbnails({
-          src: `https://image.mux.com/${src}/storyboard.vtt`,
+          src: storyboardUrl,
         });
       }
 
+      let playbackUrl = `https://stream.mux.com/${src}`;
+      
+      // check if we have a video token to use with signed playback URLs
+      if (player.options().playbackToken) {
+        playbackUrl = playbackUrl + `?token=` + player.options().playbackToken;
+      }
+      
       next(null, {
-        src: `https://stream.mux.com/${src}`,
+        src: playbackUrl,
         type: 'application/x-mpegurl',
       });
 
