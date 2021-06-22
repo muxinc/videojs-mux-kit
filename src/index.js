f7b7fa23223e72cd5e8c5e8dc1617f91eb54e861
james-mux <james@mux.com>
2021-06-22 17:33:26 +0100

    support signed urls

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import './tech/hlsjs';
 import {setupMuxDataTracking, setupMuxDataMetadataOverride} from './utils/mux-data-middleware';
 import {setupSubtitlesForPlayer} from './utils/mux-subtitles';
 
+let vttThumbnailsInitialized = false;
+
 videojs.hook('beforesetup', function(videoEl, options) {
   // We might have Mux Data enabled, and we need to handle overriding some metadata
   options = setupMuxDataMetadataOverride(videoEl, options);
@@ -22,10 +24,21 @@ videojs.use('video/mux', (player) => {
         // strip off any playback related query string parameters, so the
         // storyboard url is not malformed
         let playbackId = src.split(`?`, 1);
+        let storyboardUrl = `https://image.mux.com/${playbackId[0]}/storyboard.vtt`;
+
+        if (player.options().timelineHoverPreviews.token) {
+          // append the token to the storyboard request, if a token is provided
+          storyboardUrl = storyboardUrl + `?token=` + player.options().timelineHoverPreviews.token;
+        }
         
-        player.vttThumbnails({
-          src: `https://image.mux.com/${playbackId[0]}/storyboard.vtt`,
-        });
+        if (!vttThumbnailsInitialized) {
+          // this is the first time the video is loaded, so setup the plugin
+          player.vttThumbnails({src: storyboardUrl});
+          vttThumbnailsInitialized = true;
+        } else {
+          // we already have a vttThumbnails plugin running, so just update the src
+          player.vttThumbnails.src(storyboardUrl);
+        }
       }
 
       next(null, {
