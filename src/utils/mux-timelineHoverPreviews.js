3acebfe460d832b35600229402da48a426e9c60d
james-mux <james@mux.com>
2021-07-09 12:20:04 +0100

    fix ssr issues

--- src/utils/mux-timelineHoverPreviews.js
+++ src/utils/mux-timelineHoverPreviews.js
@@ -1,7 +1,5 @@
 import '../plugins/vtt-thumbnails.js';
 
-let vttThumbnailsInitialized = false;
-
 function setupTimelineHoverPreviewsHelper(player) {
 
   player.timelineHoverPreviews = function(options) {
@@ -9,21 +7,20 @@ function setupTimelineHoverPreviewsHelper(player) {
     let enabled = options?.enabled || false;
     let source = options?.src || null;
   
-    if (!enabled && vttThumbnailsInitialized) {
+    if (!enabled && typeof(player.vttThumbnails.detach) === 'function') {
       // if timelineHoverPreviews were enabled, remove them
       player.vttThumbnails.detach();
       player.options().timelineHoverPreviewsUrl = null;
       return;
   
-    } else if (!vttThumbnailsInitialized && source !== null) {
+    } else if (typeof(player.vttThumbnails) === 'function' && source !== null) {
       // if this is the first time the player instance has a timelineHoverPreviews source,
       // init the plugin with the source
       player.vttThumbnails({src: source});
-      vttThumbnailsInitialized = true;
       player.options().timelineHoverPreviewsUrl = source;
       return;
   
-    } else if (vttThumbnailsInitialized && source !== null) {
+    } else if (typeof(player.vttThumbnails.src) === 'function'&& source !== null) {
       // the plugin is already running, so just update to the new source
       player.vttThumbnails.src(source);
       player.options().timelineHoverPreviewsUrl = source;
