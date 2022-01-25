ffae6b5d52792216941f338f6eb651c7a5ed2951
Gary Katsevman <git@gkatsev.com>
2022-01-25 18:02:53 -0500

    use VHS by default

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import videojs from 'video.js';
 import './style/index.scss';
 
-import './tech/hlsjs';
 import {setupMuxDataTracking, setupMuxDataMetadataOverride} from './utils/mux-data-middleware';
-import {setupSubtitlesForPlayer} from './utils/mux-subtitles';
 import {setupTimelineHoverPreviewsHelper} from './utils/mux-timelineHoverPreviews';
 
 videojs.hook('beforesetup', function(videoEl, options) {
