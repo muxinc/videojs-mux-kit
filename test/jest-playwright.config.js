60dc9a1b30f553c232f32ca37b03b91c9c270036
Gary Katsevman <git@gkatsev.com>
2022-01-27 17:30:22 -0500

    run tests in chrome instead of chromium, don't run vhs tests on webkit

--- test/jest-playwright.config.js
+++ test/jest-playwright.config.js
@@ -1,19 +1,36 @@
 // https://github.com/playwright-community/jest-playwright/#configuration
 
 const port = 8080;
+const usingVhs = Boolean(process.env.vhs);
 
 module.exports = {
-  browsers: ["chromium", "firefox", "webkit"],
+  browsers: [
+    {
+      browserType: "chromium",
+      launchOptions: {
+        // run against the chrome channel so that the proper media codecs are available
+        channel: 'chrome'
+      }
+    },
+    "firefox"
+  ],
   exitOnPageError: false,
   launchOptions: {
     headless: true
   },
   serverOptions: {
     port,
-    command: 'npm run dev' + (Boolean(process.env.vhs) ? ':vhs' : ''),
+    command: 'npm run dev' + (usingVhs ? ':vhs' : ''),
     launchTimeout: 180000,
     waitOnScheme: {
       resources: [`http://localhost:${port}`],
     },
   }
+}
+
+if (!usingVhs) {
+  // VHS doesn't run properly on webkit
+  // `usingVhs ? '' : 'webkit'` in the `browsers` array above won't work
+  // because playwright will use `chromium` as the default when give `''`
+  module.exports.browsers.push('webkit');
 }
