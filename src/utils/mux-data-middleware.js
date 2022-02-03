7aaaabeb6c9784b35c8e8622b4782a616619b5eb
Gary Katsevman <git@gkatsev.com>
2022-02-03 13:54:02 -0500

    append -vhs to player software name for the VHS builds

--- src/utils/mux-data-middleware.js
+++ src/utils/mux-data-middleware.js
@@ -36,7 +36,8 @@ function setupMuxDataMetadataOverride (videoEl, options) {
 }
 
 function injectMuxDataMetadata (options) {
-  options.plugins.mux.data["player_software_name"] = "vjs-mux-kit-" + videojs?.VERSION.split('.')[0];
+  // The default build includes Hls and it's a function, in the VHS build Hls.js is excluded and it's an empty object
+  options.plugins.mux.data["player_software_name"] = "vjs-mux-kit-" + videojs?.VERSION.split('.')[0] + (typeof Hls === 'function' ? '' : '-vhs');
   options.plugins.mux.data["player_software_version"] = pkg.version;
   
   return options;
