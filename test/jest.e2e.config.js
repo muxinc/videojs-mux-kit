0c3566327aa940b2d5b616944513a4bf18f9b80e
Alex de Chaves <adechaves@mux.com>
2025-08-01 12:31:01 -0400

    update tests

--- test/jest.e2e.config.js
+++ test/jest.e2e.config.js
@@ -3,6 +3,6 @@ https://jestjs.io/docs/configuration
 module.exports = {
   preset: 'jest-playwright-preset',
   testMatch: ["**/e2e/**.test.js"],
-  testTimeout: 30000,
+  testTimeout: 50000,
   verbose: true,
 };
